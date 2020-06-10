750b9ce74e397a48a9460a6da2e58e94cc908b28
Judele, Daniel <judeih@yahoo.com>
2020-06-10 21:55:12 +0300

    Added "Introduce Parameter Object" method

---
+++ test/introduceParameterObject.test.js
@@ -0,0 +1,44 @@
+var assert = require("assert");
+var Object = require("../src/Introduce Parameter Object/introduceParameterObject");
+var personObject = new Object.Person("Dan","Popescu", new Date(2002, 12, 01));
+var addressObject = new Object.Address("Route","66","New York","United States");
+
+describe("Introduce Parameter Object", function () {
+    describe("Constructor Test", function () {
+        it("Object is created", function () {
+            assert(personObject);
+        }),
+        it("Has first Name", function () {
+            assert.equal("Dan",personObject.FirstName);
+        }),
+        it("Has last Name", function () {
+            assert.equal("Popescu",personObject.LastName);
+        }),
+        it("Has full Name", function () {
+            assert.equal("Dan Popescu",personObject.FullName);
+        }),
+        it("He is 17 age old", function () {
+            const age = personObject.getAge();
+            assert.equal(17, age);
+        }),
+        it("He is not an adult", function () {
+            assert.equal(false, personObject.isAdult());
+        })
+    }),
+    describe("Address Test", function () {
+        personObject.setAddress(addressObject);
+        var personAddress = personObject.Address;
+        it("Has street Name", function () {
+            assert.equal("Route",personAddress.StreetName);
+        })
+        it("Has street Number", function () {
+            assert.equal("66",personAddress.StreetNumber);
+        }),
+        it("Has town", function () {
+            assert.equal("New York",personAddress.Town);
+        }),
+        it("Has country", function () {
+            assert.equal("United States",personAddress.Country);
+        })
+    })
+});
